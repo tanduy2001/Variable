2d5703c90f95cec607018b17e8b83268b4fdda7d
tanduy2001 <82999662+tanduy2001@users.noreply.github.com>
2021-05-06 14:16:46 +0700

    Update MainApp.java

--- src/basic/dev/MainApp.java
+++ src/basic/dev/MainApp.java
@@ -1,9 +1,22 @@
 package basic.dev;
-
+/**
+ * Lab05
+ * @author Admin
+ *
+ */
 public class MainApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String firstName= "Phan Van";
+		String lastName= "Nam";
+		int old= 15;
+		float markMath= 9f;
+		boolean passed= true;
+		System.out.println("Ho va ten:");
+		System.out.println(firstName+ " "+ lastName);
+		System.out.println("Mark Math: "+ markMath);
+		System.out.println("Passed: "+ passed);
+		final float PI= 3.14f;
 
 	}
 
